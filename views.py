237daf656ea79f2d7db309b1016279dab809723d
Subin <ben7232@gmail.com>
2023-06-04 20:52:59 +0900

    Feat: 등락률 표시 및 게시판 기능 구현

--- views.py
+++ views.py
@@ -42,7 +42,8 @@ def get_posts():
 
         trade_post = TradePost(_id=_id, user=user, coin_count=coin_count, price=price, timestamp=timestamp, status=status)
         post_list.append(trade_post)
-
+    #날짜별 내림차순 정렬
+    post_list.sort(key=lambda x: x.timestamp, reverse=True)
     return post_list
 
 
@@ -69,7 +70,7 @@ def sell_coin():
 
             # 새로운 게시글 객체 생성
             timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            TradePost(user=session['username'], coin_count=sell_coin_count, price=sell_coin_price, timestamp=timestamp, status='판매 중').save()
+            TradePost(_id=ObjectId(), user=session['username'], coin_count=sell_coin_count, price=sell_coin_price, timestamp=timestamp, status='판매 중').save()
 
             # 게시글 목록 가져오기
             posts = get_posts()
@@ -110,6 +111,54 @@ def delete_post(post_id):
     else:
         return jsonify({'success': False, 'error_msg': "로그인이 필요합니다."})
 
+@app.route('/buy_coin_from_post/<post_id>', methods=['POST'])
+def buy_coin_from_post(post_id):
+    if 'username' in session:
+        trade_posts = mongo.db.trade_posts
+        users = mongo.db.users
+        marketplace = mongo.db.marketplace
+        market_info = marketplace.find_one()
+
+        post = trade_posts.find_one({"_id": ObjectId(post_id)})
+        if post:
+            buy_count = int(request.json['buyCount'])
+            post_user = post['user']
+            post_coin_count = post['coin_count']
+            post_coin_price = post['price']
+            post_user_balance = users.find_one({'name': post_user}).get('account_balance', 0)
+            buyer = users.find_one({'name': session['username']})
+            buyer_balance = buyer.get('account_balance', 0)
+            buyer_coin_count = buyer.get('coin_count', 0)
+
+            if buy_count <= post_coin_count and buy_count * post_coin_price <= buyer_balance:
+                new_post_coin_count = post_coin_count - buy_count
+                new_post_user_balance = post_user_balance + buy_count * post_coin_price
+                new_buyer_balance = buyer_balance - buy_count * post_coin_price
+                new_buyer_coin_count = buyer_coin_count + buy_count
+
+                #마켓의 코인 수량 업데이트
+                new_market_coin_count = market_info.get('coin_count', 0) - buy_count
+                marketplace.update_one({}, {'$set': {'coin_count': new_market_coin_count}})
+
+                # 판매자의 잔고 업데이트
+                users.update_one({'name': post_user}, {'$set': {'account_balance': new_post_user_balance}})
+                # 구매자의 잔고 및 코인 개수 업데이트
+                users.update_one({'name': session['username']}, {'$set': {'account_balance': new_buyer_balance, 'coin_count': new_buyer_coin_count}})
+
+                # 코인 데이터 추가
+                add_coin_data(post_coin_price)
+                # 게시글 상태 업데이트
+                trade_posts.update_one({"_id": ObjectId(post_id)}, {'$set': {'coin_count': new_post_coin_count}})
+                if new_post_coin_count == 0:
+                    trade_posts.update_one({"_id": ObjectId(post_id)}, {'$set': {'status': '거래 완료'}})
+
+                return jsonify({'success': True})
+            else:
+                return jsonify({'success': False, 'error_msg': "구매할 수 있는 코인 수량 또는 계정 잔고가 부족합니다."})
+        else:
+            return jsonify({'success': False, 'error_msg': "게시글을 찾을 수 없습니다."})
+
+    return jsonify({'success': False, 'error_msg': "로그인이 필요합니다."})
 
 @app.route('/trade_post', methods=['GET', 'POST'])
 def trade_post():
@@ -122,11 +171,11 @@ def initialize_marketplace():
     marketplace = mongo.db.marketplace
     marketplace.insert_one({'coin_count': 100, 'coin_price': 100})
 
+
 def add_coin_data(price):
     coin_data = mongo.db.coin_data
     timestamp = datetime.now()
     coin_data.insert_one({'price': price, 'timestamp': timestamp})
-
 def update_marketplace(coin_count, coin_price):
     marketplace = mongo.db.marketplace
     marketplace.update_one({}, {'$set': {'coin_count': coin_count, 'coin_price': coin_price}})
@@ -134,15 +183,28 @@ def update_marketplace(coin_count, coin_price):
 with app.app_context():
     initialize_marketplace()
 
+
 @app.route('/')
 def home():
     marketplace = mongo.db.marketplace
     market_info = marketplace.find_one()
 
     # 코인 데이터 가져오기
-    coin_data = mongo.db.coin_data.find({}, {"_id": 0, "timestamp": 1, "price": 1}).sort("timestamp", 1)
+    coin_data_db = mongo.db.coin_data
+    coin_data = coin_data_db.find({}, {"_id": 0, "timestamp": 1, "price": 1}).sort("timestamp", 1)
     coin_labels = []
     coin_prices = []
+
+    # 마지막 두 거래의 가격을 가져옵니다.
+    last_two_prices = list(coin_data_db.find().sort([('timestamp', -1)]).limit(2))
+
+    if len(last_two_prices) < 2:
+        # 이전 거래가 없으면 변화율을 0으로 설정합니다.
+        coin_change_rate = 0
+    else:
+        # 가장 최근 거래와 그 이전 거래의 가격 차이를 계산합니다.
+        coin_change_rate = round(
+            ((last_two_prices[0]['price'] - last_two_prices[1]['price']) / last_two_prices[1]['price']) * 100, 2)
 
     # 시간대와 가격 데이터 추출 및 포맷팅
     for data in coin_data:
@@ -158,9 +220,16 @@ def home():
         user = users.find_one({'name': session['username']})
         coin_count = user.get('coin_count', 0)
         account_balance = user.get('account_balance', 0)
-        return render_template('home.html', coin_count=coin_count, account_balance=account_balance, coin_labels=coin_labels, coin_prices=coin_prices, market_coin_count=market_info.get('coin_count', 0), market_coin_price=market_info.get('coin_price', 100))
+        return render_template('home.html', coin_change_rate=coin_change_rate, coin_count=coin_count,
+                               account_balance=account_balance, coin_labels=coin_labels, coin_prices=coin_prices,
+                               market_coin_count=market_info.get('coin_count', 0),
+                               market_coin_price=market_info.get('coin_price', 100))
     else:
-        return render_template('home.html', coin_labels=coin_labels, coin_prices=coin_prices, market_coin_count=market_info.get('coin_count', 0), market_coin_price=market_info.get('coin_price', 100))
+        return render_template('home.html', coin_change_rate=coin_change_rate, coin_labels=coin_labels,
+                               coin_prices=coin_prices, market_coin_count=market_info.get('coin_count', 0),
+                               market_coin_price=market_info.get('coin_price', 100))
+
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
@@ -257,7 +326,6 @@ def buy_coin():
             users.update_one({'name': session['username']}, {'$set': {'account_balance': new_balance, 'coin_count': new_user_coin_count}})
             marketplace.update_one({}, {'$set': {'coin_count': new_market_coin_count}})
 
-            # 코인 데이터 추가
             add_coin_data(coin_price)
 
             return jsonify({'success': True})
@@ -266,5 +334,7 @@ def buy_coin():
 
     return jsonify({'success': False, 'error_msg': "로그인이 필요합니다."})
 
+
 if __name__ == '__main__':
     app.run()
+
